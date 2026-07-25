2a5275c3928fd55e2464363755098defda26b0dc
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2026-07-25 09:08:46 -0700

    fix(auth): pass returnTo option to signOut to redirect to /login

--- src/app/sign-out/route.ts
+++ src/app/sign-out/route.ts
@@ -2,7 +2,9 @@ import { signOut } from "@workos-inc/authkit-nextjs";
 import { cookies } from "next/headers";
 import { tokenCookieName } from "@/lib/server-auth";
 
-export async function GET() {
+export async function GET(request: Request) {
   (await cookies()).delete(tokenCookieName);
-  await signOut();
+  const url = new URL(request.url);
+  const returnTo = `${url.protocol}//${url.host}/login`;
+  await signOut({ returnTo });
 }
